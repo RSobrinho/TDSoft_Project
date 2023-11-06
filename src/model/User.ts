4f31b009825274a678b91280347546f3d58da5b3
Eduardo Santos Luz <eduardo.luz@compasso.com.br>
2023-11-06 19:52:59 -0400

    add: eslint and prettier

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,13 +1,13 @@
 export class User {
-    private name: string;
-    private age: number;
+  private name: string;
+  private age: number;
 
-    constructor(name: string, age: number) {
-        this.name = name
-        this.age = age
-    }
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
 
-    public sayMyThings(): String {
-        return `My name is ${this.name} and my age is ${this.age}`
-    }
+  public sayMyThings(): String {
+    return `My name is ${this.name} and my age is ${this.age}`;
+  }
 }
